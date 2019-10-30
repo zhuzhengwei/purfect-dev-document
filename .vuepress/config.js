8f0e35db514912c9e949b556cef52ddffdd8d9a8
vagrant <vagrant@homestead>
2019-10-30 08:00:36 +0000

    公文接口

---
+++ .vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    title: "后台管理系统设计文档",
+    markdown: {
+        lineNumbers: false
+    },
+    themeConfig: {
+        // 顶部导航栏
+        nav: [
+            {text: '首页', link: '/'},
+            {
+                text: '接口设计',link: '/api/'
+            },
+            {
+                text: '业务逻辑的实现', link:'/business_logic/'
+            },
+            {
+                text: '数据库设计',link: '/database/'
+            }
+        ],
+        // 侧边栏
+        sidebar: 'auto'
+    }
+}
